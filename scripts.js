37f0d6b5115f99bcfae23ddf88417656aa8c1a6b
Taiany35 <128820487+Taiany35@users.noreply.github.com>
2024-06-14 08:55:53 -0300

    PROVA-18
    PROVA-18

--- scripts.js
+++ scripts.js
@@ -3,5 +3,13 @@ function inscrever(){
     const heigth = 600;
     const left = (screen.width / 2) - (width / 2);
     const top = (screen.heigth / 2) - (heigth / 2);
-    window.open('pagina4.html', 'pagamento', 'width=$(width), heigth=$(heigth), top=$(top), lefth')
+    window.open('pagina4.html', 'pagamento', 'width=${width}, heigth=${heigth}, top=${top}, lefth=${left}');
+}
+
+function Reserva(){
+    const width = 800;
+    const heigth = 600;
+    const left = (screen.width / 2) - (width / 2);
+    const top = (screen.heigth / 2) - (heigth / 2);
+    window.open('pagina5.html', 'pagamento', 'width=${width}, heigth=${heigth}, top=${top}, lefth=${left}');
 }
